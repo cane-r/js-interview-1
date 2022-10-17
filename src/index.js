3933769d99accd6c13b7412d84a1ae2aef23c4f0
Caner Siris <canersir@gmail.com>
2022-10-17 14:17:32 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -5,6 +5,34 @@ export default class MoviesAnalyzer {
     }
 
     topRatedMoviesAmongFriends(userId) {
-        // TODO: Implement
+        const user = this.users.filter(f => f.userId === userId);
+        const friends = user[0].friends;
+        const res = [];
+
+        for (let movie of this.movies) {
+            movie.ratings2 = [];
+            for (let rating of movie.ratings) {
+                for (let friend of friends) {
+                    if (rating.userId == friend) {
+                        movie.ratings2.push(rating);
+                    }
+                }
+            }
+            if (movie.ratings2.length > 0) {
+                movie.avg = movie.ratings2.reduce((accumulator, object) => {
+                    return accumulator + object.rating;
+                }, 0) / movie.ratings2.length;
+                res.push(movie);
+            } else {
+                delete movie.ratings2;
+            }
+        }
+        res.sort((m1, m2) => {
+            if (m2.avg == m1.avg) {
+                return m1.title > m2.title ? 1 : -1;;
+            }
+            return m2.avg - m1.avg;
+        });
+        return res.map(m => m.title).slice(0, 3);
     }
 }
