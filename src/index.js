c81a40f938a82fce59c4777b04500d7d277ee873
Caner Siris <canersir@gmail.com>
2022-10-17 14:16:08 +0300

    initial commit : project skeleton

---
+++ src/index.js
@@ -0,0 +1,10 @@
+export default class MoviesAnalyzer {
+    constructor(movies, users) {
+        this.movies = movies;
+        this.users = users;
+    }
+
+    topRatedMoviesAmongFriends(userId) {
+        // TODO: Implement
+    }
+}
